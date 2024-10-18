280e1555de35bb0a1f6b075b9325941957800bc3
Ernur Torekul <ernurtorekul@gmail.com>
2024-10-18 09:22:52 +0500

    added all 3

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,32 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [user, setUser] = useState(null);
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?15';
-    script.setAttribute('data-telegram-login', 'userCech_bot');  // Replace with your bot's username
+    script.setAttribute('data-telegram-login', 'userCech_bot');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-auth-url', '/api/verifyTelegram');
     script.setAttribute('data-request-access', 'write');
     script.async = true;
-
+    
+    script.onload = () => {
+      if (window.Telegram?.WebApp) {
+        const telegram = window.Telegram.WebApp;
+        const user = telegram.initDataUnsafe?.user;
+        if (user) {
+          setUser(user);
+        }
+        telegram.ready();
+      }
+    };
+    
     document.getElementById('telegram-login').appendChild(script);
   }, []);
+  
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.Telegram?.WebApp) {
+    if (typeof window !== "undefined" && window.Telegram && window.Telegram.WebApp) {
       const telegram = window.Telegram.WebApp;
       const user = telegram.initDataUnsafe?.user;
       if (user) {
@@ -26,6 +37,7 @@ export default function Home() {
       telegram.ready(); // Notify Telegram that the app is ready
     }
   }, []);
+  
 
   return (
     <div>
