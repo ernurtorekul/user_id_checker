4f8510d11e9296850814aa06551e4a75e268eb0c
Ernur Torekul <ernurtorekul@gmail.com>
2024-10-18 09:28:26 +0500

    added all 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // This applies to all routes
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Permissions-Policy',
+              value: 'interest-cohort=()', // Disable FLoC or any unnecessary features
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
