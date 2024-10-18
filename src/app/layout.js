51810d5e288279007b97eac56568319c84076aba
Ernur Torekul <ernurtorekul@gmail.com>
2024-10-18 12:05:14 +0500

    added all 5

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+
+        </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
